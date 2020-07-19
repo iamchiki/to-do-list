bef5d0bfe80caa29a06e40ac5e92a06e9c220e40
iamchiki <nikhil.soni599@gmail.com>
2020-07-19 15:56:35 +0530

    added event listner

---
+++ app.js
@@ -0,0 +1,11 @@
+//Selectors
+let todoButton = document.querySelector('.todo-button')
+
+//Event listerner
+todoButton.addEventListener('click', addTodo);
+
+
+//functions
+function addTodo(event) {
+  console.log('hello');
+}
