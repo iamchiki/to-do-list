9f5564ec371d19e0e779f5fe67d194acf1b27991
iamchiki <nikhil.soni599@gmail.com>
2020-07-20 10:08:55 +0530

    Add Functionality

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //Selectors
-let todoButton = document.querySelector('.todo-button')
+let todoButton = document.querySelector('.todo-button');
+let todoInput = document.querySelector('.todo-input');
+let todoList = document.querySelector('.todo-list');
 
 //Event listerner
 todoButton.addEventListener('click', addTodo);
@@ -7,5 +9,35 @@ todoButton.addEventListener('click', addTodo);
 
 //functions
 function addTodo(event) {
-  console.log('hello');
+
+  //prevent form submission
+  event.preventDefault();
+
+  // creat div
+  let todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+
+  // creat Li
+  let liItem = document.createElement('li');
+  liItem.classList.add('todo-item');
+  liItem.innerText = todoInput.value;
+  todoDiv.append(liItem);
+
+  // creat check Button
+  let checkButton = document.createElement('button');
+  checkButton.classList.add('complete-btn');
+  checkButton.innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>';
+  todoDiv.append(checkButton);
+
+  // creat trash Button
+  let trashButton = document.createElement('button');
+  trashButton.classList.add('trash-btn');
+  trashButton.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
+  todoDiv.append(trashButton);
+
+  // append div to ul
+  todoList.append(todoDiv);
+
+  // clear input after padding
+  todoInput.value = '';
 }
